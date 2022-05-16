a64fdc1fc4ba8204625fb684c972226ba4651d9b
CDCODE <99984839+CDCODE02@users.noreply.github.com>
2022-05-17 00:59:40 +0100

    first commit
    lettuce project v1 full site

---
+++ lettuceWebV1/lettuce.js
@@ -0,0 +1,6 @@
+const navLink = document.querySelectorALL(".nav-link");
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
